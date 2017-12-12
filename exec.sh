164e85e686fb6befa5efc59f522f82dd2004b86f
Neptune Nyx Nishedcob <nishedcob@gmail.com>
2017-12-12 11:24:11 -0500

    Inicial Version of Shell Code Executor Template

---
+++ exec.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+DEBUG=false
+NULL="/dev/null"
+PRINT_NULL="> $NULL 2> $NULL"
+
+if $DEBUG; then
+	echo ""
+
+	whoami
+	echo "says hello world!"
+	echo "from Docker!"
+
+	echo ""
+
+	lsb_release -a
+	uname -a
+
+	echo ""
+
+	cat /etc/os-release
+
+	echo ""
+fi
+
+./main.sh
+
